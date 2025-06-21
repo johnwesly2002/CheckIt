c8e9db52540890ae0f0233437e2c9dff1e8cb68a
johnwesly2002 <ujohnwesly8@gmail.com>
2025-06-21 18:10:29 +0530

    Initial commit: Spring Boot app setup

---
+++ src/test/java/com/ecommerce/checkIt/CheckItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.checkIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
